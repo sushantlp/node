3e9ce6fed806ccebd74bb4dae937acc8a25d27a1
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2018-11-09 20:06:06 -0300

    fix lint

--- tests/stress/createClaimAPI.ts
+++ tests/stress/createClaimAPI.ts
@@ -2,18 +2,19 @@ const Koa = require('koa')
 const { createACDClaim } = require('../helpers/createClaims')
 
 const main = async () => {
-  const app = new Koa();
+  const app = new Koa()
 
   app.use(async (ctx: any) => {
     const attributes = {
       name: `testing claim ${Date.now()}`,
       author: 'the tester',
     }
-  
+
     ctx.body = await createACDClaim(attributes)
   })
-  
+
   app.listen(3000)
 }
 
+// tslint:disable:no-console
 main().catch(console.error)
